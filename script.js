6265dc5e421929c1ceb0085ba812973e9adff8f1
Toni Williams <williata04@mail.buffalostate.edu>
2022-11-05 19:04:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-document.querySelector("button").addEventListener("click", getDrink);
+document.querySelector("#enter").addEventListener("click", getDrink);
 let drinkArray = [];
 let name = document.querySelector(".name");
-let type = document.querySelector(".type");
 let img = document.querySelector("img");
 let instrucs = document.querySelector(".p");
 let ingreds = document.querySelector(".ingredients");
@@ -23,9 +22,8 @@ function getDrink() {
         drinkArray.push(data.drinks[i]);
       }
       console.log(drinkArray, drinkArray[0].strDrink);
-
+document.querySelector('#card').style.display="block"
       name.innerText = drinkArray[0].strDrink;
-      type.innerText = drinkArray[0].strAlcoholic;
       img.src = drinkArray[0].strDrinkThumb;
       instrucs.innerText = drinkArray[0].strInstructions;
       ingreds.innerText = drinkArray[0].strIngredient1;
@@ -40,10 +38,9 @@ function getDrink() {
     });
 }
 
-document.querySelector(".next-button").addEventListener("click", () => {
+document.querySelector("#next-button").addEventListener("click", () => {
   counter++; //At the start of the click event we will increase the counter by 1
   name.innerText = "";
-  type.innerText = "";
   img.src = "";
   instrucs.innerText = "";
   ingreds.innerText = "";
@@ -57,7 +54,6 @@ document.querySelector(".next-button").addEventListener("click", () => {
     //This will check if the counter has increased to be greater than the last element in the array
     counter = 0; //If the counter is greater than the last element in the array, set the counter back to zero, and make all the drink information equal to the information from the first element in the array
     name.innerText = drinkArray[counter].strDrink;
-    type.innerText = drinkArray[counter].strAlcoholic;
     img.src = drinkArray[counter].strDrinkThumb;
     instrucs.innerText = drinkArray[counter].strInstructions;
     ingreds.innerText = drinkArray[counter].strIngredient1;
@@ -69,7 +65,6 @@ document.querySelector(".next-button").addEventListener("click", () => {
   } else {
     //If the counter is not greater than the last element in the array, the drink info will the whatever the next element is
     name.innerText = drinkArray[counter].strDrink;
-    type.innerText = drinkArray[counter].strAlcoholic;
     img.src = drinkArray[counter].strDrinkThumb;
     instrucs.innerText = drinkArray[counter].strInstructions;
     ingreds.innerText = drinkArray[counter].strIngredient1;
@@ -80,10 +75,9 @@ document.querySelector(".next-button").addEventListener("click", () => {
     measure.innerText = drinkArray[counter].strMeasure1;
   }
 });
-document.querySelector(".prev-button").addEventListener("click", () => {
+document.querySelector("#prev-button").addEventListener("click", () => {
   counter--; //At the start of the click event, decrement the counter by 1
   name.innerText = "";
-  type.innerText = "";
   img.src = "";
   instrucs.innerText = "";
   ingreds.innerText = "";
@@ -97,7 +91,6 @@ document.querySelector(".prev-button").addEventListener("click", () => {
     //If the counter has been decremented below passed 0 (the start of the array), we will set the counter equal to the last element of the array
     counter = drinkArray.length - 1;
     name.innerText = drinkArray[counter].strDrink;
-    type.innerText = drinkArray[counter].strAlcoholic;
     img.src = drinkArray[counter].strDrinkThumb;
     instrucs.innerText = drinkArray[counter].strInstructions;
     ingreds.innerText = drinkArray[counter].strIngredient1;
@@ -109,7 +102,6 @@ document.querySelector(".prev-button").addEventListener("click", () => {
   } else {
     //If the counter is not less than 0, we will just find what the next element is and put its data into the DOM
     name.innerText = drinkArray[counter].strDrink;
-    type.innerText = drinkArray[counter].strAlcoholic;
     img.src = drinkArray[counter].strDrinkThumb;
     instrucs.innerText = drinkArray[counter].strInstructions;
     ingreds.innerText = drinkArray[counter].strIngredient1;
@@ -122,17 +114,16 @@ document.querySelector(".prev-button").addEventListener("click", () => {
 });
 
 document.querySelector(".random-button").addEventListener("click", () => {
- Math.floor(Math.random() * drinkArray.length)
-   name.innerText = drinkArray[counter].strDrink;
-    type.innerText = drinkArray[counter].strAlcoholic;
-    img.src = drinkArray[counter].strDrinkThumb;
-    instrucs.innerText = drinkArray[counter].strInstructions;
-    ingreds.innerText = drinkArray[counter].strIngredient1;
-    ingred2.innerText = drinkArray[counter].strIngredient2;
-    ingred3.innerText = drinkArray[counter].strIngredient3;
-    ingred4.innerText = drinkArray[counter].strIngredient4;
-    ingred5.innerText = drinkArray[counter].strIngredient5;
-    measure.innerText = drinkArray[counter].strMeasure1;
-})
+  Math.floor(Math.random() * drinkArray.length);
+  name.innerText = drinkArray[counter].strDrink;
+  img.src = drinkArray[counter].strDrinkThumb;
+  instrucs.innerText = drinkArray[counter].strInstructions;
+  ingreds.innerText = drinkArray[counter].strIngredient1;
+  ingred2.innerText = drinkArray[counter].strIngredient2;
+  ingred3.innerText = drinkArray[counter].strIngredient3;
+  ingred4.innerText = drinkArray[counter].strIngredient4;
+  ingred5.innerText = drinkArray[counter].strIngredient5;
+  measure.innerText = drinkArray[counter].strMeasure1;
+});
 
 
